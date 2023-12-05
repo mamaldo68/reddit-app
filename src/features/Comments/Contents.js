c93ffa5f4c1f73137bdf8c1e9d26574ad6dc281c
mamaldo <mamaldo68@gmail.com>
2023-12-05 18:48:46 -0500

    Non album imgur links now display

--- src/features/Comments/Contents.js
+++ src/features/Comments/Contents.js
@@ -7,12 +7,12 @@ const Contents = () => {
     const contents = useSelector(selectContents);
     
     if (contents && contents.data) {
-        const { title, author, url, selftext, thumbnail} = contents.data;
+        const { title, author, url, selftext, thumbnail, domain } = contents.data;
         return (
             <div>
                 <p>{title}</p>
                 <p>Submitted by: {author}</p>
-                {url && thumbnail !== "self" && <img src={url}/>}
+                {url && thumbnail !== "self" && <img src={domain === "imgur.com" ? url + ".png" : url}/>}
                 {selftext && <MarkdownRenderer markdownContent={selftext}/>}
             </div>
         );
