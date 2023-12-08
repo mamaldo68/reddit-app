2b5a0b004f69ebb54dc5c5d9df9a6c0b9e4a7b2b
mamaldo <mamaldo68@gmail.com>
2023-12-08 02:15:58 -0500

    Add default posts to display along with ways to get back to it and ability to go to a subreddit from post tile. More style changes

--- src/Root/Root.js
+++ src/Root/Root.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SearchBar from "../SearchBar/SearchBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { loadPosts } from "../features/Posts/postsSlice";
+import { addSubreddit, addSortType } from "../features/Subreddit/subredditSlice";
 import styles from "./Root.module.css";
 
+
 const Root = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    useEffect(() => {
+        dispatch(addSubreddit("all"));
+        dispatch(addSortType("hot"));
+        dispatch(loadPosts({subreddit: "all", sortType: "hot"}));
+        navigate(`r/all/hot`);
+    }, []);
+
+    const clickHandler = () => {
+        dispatch(addSubreddit("all"));
+        dispatch(addSortType("hot"));
+        dispatch(loadPosts({subreddit: "all", sortType: "hot"}));
+        navigate(`r/all/hot`);
+    };
+
     return (
         <div>
             <div  className={styles.container}>
-                <h1>Reddit <span className={styles.accent}>Lite</span></h1>
+                <div className={styles.clickable} onClick={clickHandler}><h1>Reddit <span className={styles.accent}>Lite</span></h1></div>
                 <SearchBar/>
             </div>
             <Outlet/>
--- src/features/Posts/Post.js
+++ src/features/Posts/Post.js
@@ -1,29 +1,45 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadComments } from "../Comments/commentsSlice";
+import { loadPosts } from "./postsSlice";
+import { selectSortType, addSubreddit } from "../Subreddit/subredditSlice";
 import { decodeUrl } from "../../util/mediaHelperFunctions";
 import styles from "./Post.module.css";
 
 const Post = ({ post, counter }) => {
     const utcTime = post.data.created_utc;
     const date = new Date(utcTime * 1000);
+    const sortType = useSelector(selectSortType);
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
-    const clickHandler = (object) => {
+    const commentClickHandler = (object, event) => {
         dispatch(loadComments(object.permalink));
         navigate(`${object.permalink}`);
+        event.stopPropagation();
+    };
+
+    const subredditClickHandler = (object, event) => {
+        dispatch(loadPosts({subreddit: object.subreddit, sortType: sortType}));
+        dispatch(addSubreddit(object.subreddit));
+        navigate(`/r/${object.subreddit}/${sortType}`);
+        event.stopPropagation();
     };
 
     return (
         <div className={styles.container}>
             <p className={styles.count}>{counter}</p>
             {post.data.thumbnail !== "self" && post.data.thumbnail !== "spoiler" && <img className={styles.thumbnail} src={decodeUrl(post.data.thumbnail)}></img>}
-            <div className={styles.infoContainer} onClick={() => clickHandler(post.data)}>
+            <div className={styles.infoContainer} onClick={(event) => commentClickHandler(post.data, event)}>
                 <p className={styles.title}>{post.data.title}</p> 
                 <p className={styles.info}>
-                    By: <span className={post.data.distinguished === "moderator" || post.data.author === "AutoModerator" ? styles.mod : ""}>{post.data.author}</span> to r/{post.data.subreddit}
+                    By: <span className={post.data.distinguished === "moderator" || post.data.author === "AutoModerator" ? styles.mod : ""}>
+                            {post.data.author + ' '}
+                        </span> 
+                    to <span className={styles.subreddit} onClick={(event) => subredditClickHandler(post.data, event)}>
+                            r/{post.data.subreddit}
+                        </span>
                 </p>
                 <p className={styles.moreInfo}>score: {post.data.score} | comments: {post.data.num_comments} | {date.toUTCString()}</p>
             </div> 
